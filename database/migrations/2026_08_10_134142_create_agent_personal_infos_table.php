5244790fbf18dd3b72307b900a63876af1bb5386
tech-riad <tech-riad45@gmail.com>
2026-08-10 13:49:31 +0600

    update

---
+++ database/migrations/2026_08_10_134142_create_agent_personal_infos_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAgentPersonalInfosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('agent_personal_infos', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('agent_id');
+            $table->timestamp('date_of_birth')->nullable();
+            $table->string('profession')->nullable();
+            $table->string('known_from')->nullable();
+            $table->string('institute')->nullable();
+            $table->string('institute_category')->nullable();
+            $table->string('institute_designation')->nullable();
+            $table->string('work_experience')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('agent_personal_infos');
+    }
+}
