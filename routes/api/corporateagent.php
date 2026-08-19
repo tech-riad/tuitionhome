60cf112ed0997cc1f8d10d467587ebe3042b549a
tech-riad <tech-riad45@gmail.com>
2026-08-19 16:05:46 +0600

    update

--- routes/api/corporateagent.php
+++ routes/api/corporateagent.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Route;
 Route::post('/corporate-agent/register', [CorporateAgentAuthController::class, 'register']);
 Route::post('/corporate-agent/login', [CorporateAgentAuthController::class, 'login']);
 Route::post('/corporate-agent/phone-verified', [CorporateAgentAuthController::class, 'verifyPhone']);
-Route::post('/corporate-agent/resend-otp', [CorporateAgentAuthController::class, 'resendOtp']);
+Route::post('/corporate-agent/register/resend/otp', [CorporateAgentAuthController::class, 'resendOtp']);
 
 
 Route::post('/corporate-agent/forgot-password',[CorporateAgentAuthController::class,'checkPhone']);
