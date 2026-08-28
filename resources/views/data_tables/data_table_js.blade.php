01db228c8d405aec1ef29916dcd70a57639d4158
tech-riad <tech-riad45@gmail.com>
2026-08-29 02:45:11 +0600

    update

--- resources/views/data_tables/data_table_js.blade.php
+++ resources/views/data_tables/data_table_js.blade.php
@@ -1,17 +1,17 @@
 @push('page_scripts')
 <!-- DataTables  & Plugins -->
-<!--<script src="{{asset('/backend')}}/plugins/datatables/jquery.dataTables.min.js"></script>-->
-<!--<script src="{{asset('/backend')}}/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>-->
-<!--<script src="{{asset('/backend')}}/plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>-->
-<!--<script src="{{asset('/backend')}}/plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>-->
-<!--<script src="{{asset('/backend')}}/plugins/datatables-buttons/js/dataTables.buttons.min.js"></script>-->
-<!--<script src="{{asset('/backend')}}/plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script>-->
+<script src="{{asset('/backend')}}/plugins/datatables/jquery.dataTables.min.js"></script>
+<script src="{{asset('/backend')}}/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
+<script src="{{asset('/backend')}}/plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
+<script src="{{asset('/backend')}}/plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
+<script src="{{asset('/backend')}}/plugins/datatables-buttons/js/dataTables.buttons.min.js"></script>
+<script src="{{asset('/backend')}}/plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
 <script src="{{asset('/backend')}}/plugins/jszip/jszip.min.js"></script>
 <script src="{{asset('/backend')}}/plugins/pdfmake/pdfmake.min.js"></script>
 <script src="{{asset('/backend')}}/plugins/pdfmake/vfs_fonts.js"></script>
-<!--<script src="{{asset('/backend')}}/plugins/datatables-buttons/js/buttons.html5.min.js"></script>-->
-<!--<script src="{{asset('/backend')}}/plugins/datatables-buttons/js/buttons.print.min.js"></script>-->
-<!--<script src="{{asset('/backend')}}/plugins/datatables-buttons/js/buttons.colVis.min.js"></script>-->
+<script src="{{asset('/backend')}}/plugins/datatables-buttons/js/buttons.html5.min.js"></script>
+<script src="{{asset('/backend')}}/plugins/datatables-buttons/js/buttons.print.min.js"></script>
+<script src="{{asset('/backend')}}/plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
 
 
 
