2dd5260c0eed79bf59c9627b1890e68831a88a5d
tech-riad <tech-riad45@gmail.com>
2026-08-31 17:54:16 +0600

    update

--- resources/views/data_tables/data_table_js.blade.php
+++ resources/views/data_tables/data_table_js.blade.php
@@ -2,9 +2,9 @@
 <!-- DataTables  & Plugins -->
 <script src="{{asset('/backend')}}/plugins/datatables/jquery.dataTables.min.js"></script>
 <script src="{{asset('/backend')}}/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
-<script src="{{asset('/backend')}}/plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
+<script src="{{asset('/backend')}}/plugins/datatables-responsive/js/dataTables.responsive.js"></script>
 <script src="{{asset('/backend')}}/plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
-<script src="{{asset('/backend')}}/plugins/datatables-buttons/js/dataTables.buttons.min.js"></script>
+<script src="{{asset('/backend')}}/plugins/datatables-buttons/js/dataTables.buttons.js"></script>
 <script src="{{asset('/backend')}}/plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
 <script src="{{asset('/backend')}}/plugins/jszip/jszip.min.js"></script>
 <script src="{{asset('/backend')}}/plugins/pdfmake/pdfmake.min.js"></script>
